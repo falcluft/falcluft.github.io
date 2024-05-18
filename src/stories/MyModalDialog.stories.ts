a82454b14ba4464189709c45bee0641626d0f7f9
falcluft <zubko_v@list.com>
2024-05-18 20:58:46 +0300

    Починка билда 13

--- src/stories/MyModalDialog.stories.ts
+++ src/stories/MyModalDialog.stories.ts
@@ -17,6 +17,6 @@ export default meta;
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const Main = {
   args: {
-    text: 'Some text',
+    //text: 'Some text',
   },
 };
