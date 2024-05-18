021b4a807dc1da613b0cfaac29e4b8c01a8b4b2e
falcluft <zubko_v@list.com>
2024-05-18 20:05:53 +0300

    Эксперименты с импортом MyButton в App

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import MyButton from './MyButton';
+import MyButton from '../stories/MyButton';
 
 function App() {
   return (
