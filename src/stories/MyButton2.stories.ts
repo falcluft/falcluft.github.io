dbe188ceb124c1d7a6fee44dc4a1d516c813006c
falcluft <zubko_v@list.com>
2024-05-18 14:09:02 +0300

    Добавил MyButton2

---
+++ src/stories/MyButton2.stories.ts
@@ -0,0 +1,22 @@
+import type { Meta } from '@storybook/react';
+
+import { MyButton2 } from './MyButton2';
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
+const meta: Meta<typeof MyButton2> = {
+  title: 'Example/MyButton2',
+  component: MyButton2,
+  tags: ['autodocs'],
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+};
+
+export default meta;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Green = {
+  args: {
+    label: 'Button green',
+  },
+};
---
+++ src/stories/MyButton2.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+interface MyButto2nProps {
+	label: string;
+}
+
+export function MyButton2({ label, ...props }: MyButton2Props){
+	return
+	(
+		<button type="button">
+		{label}
+		</button>
+	);
+}
