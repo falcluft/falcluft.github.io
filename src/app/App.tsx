081881255a5c017349032a53e3c6120a615478f5
falcluft <zubko_v@list.com>
2024-05-21 00:40:53 +0300

    Переместил вниз MyModalDialog

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -7,13 +7,14 @@ import { MyModalDialog } from '../stories/MyModalDialog';
 function App() {
   return (
       <div>
-        <MyModalDialog text="Hello! I am a moal dialog!" />
+       
         <div className="App1">
             <p>
                 Тест
             </p>
             <MyButton label="Hello 2" />
         </div>
+         <MyModalDialog text="Hello! I am a moal dialog!" />
     </div>
   );
 }
